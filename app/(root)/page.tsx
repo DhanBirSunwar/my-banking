3eec51ea8b777a08a933575070c9a38b551a9c9a
Dhan Bir Sunwar <deanvr42@gmail.com>
2024-05-09 17:22:24 +0600

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,33 @@
 import Image from "next/image";
+import HeaderBox from "../../components/HeaderBox";
+import TotalBalanceBox from "../../components/TotalBalanceBox";
+import RightSidebar from "../../components/RightSidebar";
 
 export default function Home() {
-  return <main>welcome</main>;
+  const loggedIn = {
+    firstName: "Dhan",
+    lastName: "Bir",
+    email: "dhan@dhan.com",
+  };
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || "Guest"}
+            subtext='Access and manage your account and transaction efficiently.'
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.53}
+          />
+        </header>
+        recent transaction
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+    </section>
+  );
 }
